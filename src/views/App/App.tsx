6d0e40534f7e560c2572a7464578b1cc6c6ee7fd
Jonathon Tech <jonathon.tech@icfnext.com>
2023-05-09 16:09:25 -0500

    Add settings to support TW auto-completion

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,7 +1,17 @@
+import { $ } from 'utils/classes';
+
+const styles = {
+  span: $`
+    text-3xl
+    font-bold
+    underline
+  `
+};
+
 export const App: FC = () => {
   return (
     <div>
-      <span className='text-3xl font-bold underline'>Hello world!</span>
+      <span className={styles.span}>Hello world!</span>
       <div>
         <button type='button'>Click me!</button>
       </div>
